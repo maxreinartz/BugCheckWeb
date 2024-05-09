e0b78fce86b63bdcff7761022db72e32417807cb
Creeper76 <86135033+Creeper76@users.noreply.github.com>
2024-05-09 18:10:14 -0400

    Formated my code to make it more readable, allowed changing of more elements, changed qrcode to image, refactored the sizing, changed everything to go by px instead of em

--- bugcheck.js
+++ bugcheck.js
@@ -1,54 +1,68 @@
-window.onload = function() {
-    var urlParams = new URLSearchParams(window.location.search);
-    var faceParam = urlParams.get('face');
-    var titleParam = urlParams.get('title');
-    var messageParam = urlParams.get('message');
-    var codeParam = urlParams.get('code');
-    var infoParam = urlParams.get('info');
-    var qrCodeParam = urlParams.get('qrCode');
-    var percentageParam = urlParams.get('percentage');
+window.onload = function () {
+  var urlParams = new URLSearchParams(window.location.search);
+  var faceParam = urlParams.get("face");
+  var titleParam = urlParams.get("title");
+  var messageParam = urlParams.get("message");
+  var codeParam = urlParams.get("code");
+  var infoParam = urlParams.get("info");
+  var imgParam = urlParams.get("img");
+  var percentageParam = urlParams.get("percentage");
 
-    if (faceParam) {
-        document.getElementById('bugcheck-face').innerText = faceParam;
-    }
+  var bgColorParam = urlParams.get("bgColor");
+  var textColorParam = urlParams.get("textColor");
 
-    if (titleParam) {
-        document.getElementById('bugcheck-title').innerText = titleParam;
-    }
+  if (faceParam) {
+    document.getElementById("bugcheck-face").innerText = faceParam;
+  }
 
-    if (messageParam) {
-        document.getElementById('bugcheck-message').innerText = messageParam;
-    }
+  if (titleParam) {
+    document.getElementById("bugcheck-title").innerText = titleParam;
+  }
 
-    if (codeParam) {
-        document.getElementById('stop-code').innerText = codeParam;
+  if (messageParam) {
+    document.getElementById("bugcheck-message").innerText = messageParam;
+  }
+
+  if (codeParam) {
+    document.getElementById("stop-code").innerText = codeParam;
+  } else {
+    document.getElementById("stop-code").innerText =
+      "Stop code: 0x" +
+      Math.floor(Math.random() * 0xffffff)
+        .toString(16)
+        .padStart(8, "0");
+  }
+
+  if (infoParam) {
+    document.getElementById("bugcheck-info").innerText = infoParam;
+  }
+
+  var percentage;
+  if (percentageParam) {
+    percentage = percentageParam;
+  } else {
+    percentage = Math.floor(Math.random() * 101);
+  }
+  document.getElementById("bugcheck-percentage").innerText =
+    percentage + "% complete";
+
+  if (imgParam) {
+    document.getElementById("bugcheck-img").src = imgParam;
+  } else {
+    document.getElementById("bugcheck-img").src = "assets/qrcode.png";
+  }
+
+  if (bgColorParam) {
+    bgColorParam = "#" + bgColorParam;
+    document.body.style.backgroundColor = bgColorParam;
+  } else {
+    document.body.style.backgroundColor = "#0000AA";
+  }
+
+    if (textColorParam) {
+        textColorParam = "#" + textColorParam;
+        document.body.style.color = textColorParam;
     } else {
-        document.getElementById('stop-code').innerText = 'Stop code: 0x' + Math.floor(Math.random() * 0xFFFFFF).toString(16).padStart(8, '0');
-    }
-
-    if (infoParam) {
-        document.getElementById('bugcheck-info').innerText = infoParam;
-    }
-
-    var percentage;
-    if (percentageParam) {
-        percentage = percentageParam;
-    } else {
-        percentage = Math.floor(Math.random() * 101);
-    }
-    document.getElementById('bugcheck-percentage').innerText = percentage + '% complete';
-
-    if (qrCodeParam) {
-        var qrcode = new QRCode(document.getElementById("qrcode"), {
-            text: qrCodeParam,
-            width: 128,
-            height: 128
-        });
-    } else {
-        var qrcode = new QRCode(document.getElementById("qrcode"), {
-            text: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-            width: 128,
-            height: 128
-        });
+        document.body.style.color = "#FFFFFF";
     }
 };
